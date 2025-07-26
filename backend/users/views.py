2233e25798e4f32724293ea96ae98c97fb6db10c
elvisrepo <elvis.dev2@gmail.com>
2025-07-26 17:37:33 +0200

    Api decorators views

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,64 +1,53 @@
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from users.models import User
 from users.serializers import UserSerializer
 
+
 # Create your views here.
 
-@csrf_exempt
-def users_list(request):
-    '''
+@api_view(['GET','POST'])
+def users_list(request, format=None):
+     '''
         List of all users, create a new user
     '''
-
-    if request.method == 'GET':
-        '''
-            fetch the users from DB
-            serialize them -> convert to python datatypes -> JSON
-        '''
+     if request.method == 'GET':
         users = User.objects.all()
         serializer = UserSerializer(users, many=True)
-
-        return JsonResponse(serializer.data, safe=False)
-
-    elif request.method == 'POST':
-
-        '''
-            {first_name:'visi',last_name:'bob'}
-            get data from POST body
-            validate the data?  
-            update the model
-        '''
-
-        data = JSONParser().parse(request)  # from bytestream to JSON
-        serializer = UserSerializer(data=data)  # from JSON to native python datatypes
-        if serializer.is_valid():
-            serializer.save()
-            return HttpResponse(serializer.data, status = 201)
-        return JsonResponse(serializer.errors, status = 400)
+        return Response(serializer.data)
+     
+     elif request.method == 'POST':
+         serializer = UserSerializer(data = request.data)
+         if serializer.is_valid():
+             serializer.save()
+             return Response(serializer.data, status=status.HTTP_201_CREATED)
+         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@csrf_exempt
-def user_detail(request, pk):
 
+@api_view(['GET', 'PUT', 'DELETE'])
+def user_detail(request, pk, format=None):
+    '''
+        Retrieve or delete a user
+    '''
     try:
         user = User.objects.get(pk=pk)
     except User.DoesNotExist:
-        return HttpResponse(status=404)
-    
+         return Response(status=status.HTTP_404_NOT_FOUND)
+     
     if request.method == 'GET':
-            serializer = UserSerializer(user)
-            return JsonResponse(serializer.data)
-    
+         serializer = UserSerializer(user)
+         return Response(serializer.data)
+     
     elif request.method == 'PUT':
-         data = JSONParser().parse(request)
-         serializer =  UserSerializer(user, data=data)
+    
+         serializer =  UserSerializer(user, data=user.data)
          if serializer.is_valid():
               serializer.save()
-              return JsonResponse(serializer.data)
-         return JsonResponse(serializer.errors, status=400)
+              return Response(serializer.data)
+         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)   
 
     elif request.method == 'DELETE':
         user.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
