83bc22a49593383fd7b8fb6acb3896357b64bacb
elvisrepo <elvis.dev2@gmail.com>
2025-08-17 16:18:21 +0200

    test_create_user

--- backend/users/tests.py
+++ backend/users/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
+
+User = get_user_model()
 
 # Create your tests here.
+
+class UserManagerTests(TestCase):
+
+    def test_create_user(self):
+        '''
+            Test that the custom user manager can create a user correctly.
+        '''
+        
+        user = User.objects.create_user(
+            email='test@example.com',
+            password='password123',
+            first_name='Test',
+            last_name='User'
+        )
+
+        ## assertions
+        self.assertEqual(user.email, 'test@example.com')
+        self.assertTrue(user.check_password('password123'))
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+        self.assertEqual(user.role, 'USER')
