a65ea8c7cc49748dbbdffb1ec1ae1b7fdbd2c9e7
elvisrepo <elvis.dev2@gmail.com>
2025-08-24 15:01:28 +0200

    added inverse relationship, tell the serializer to look for and include that relationship data

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,11 +1,16 @@
 from rest_framework import serializers
 from .models import User
+from accounts.models import Account
 
 
 ## Fields we want to expose in our API
 class UserSerializer(serializers.ModelSerializer):
+    
+
+    accounts = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    
     class Meta:
         model = User
-        fields = ['id', 'first_name', 'last_name', 'email', 'version', 'is_active','role']
+        fields = ['id', 'first_name', 'last_name', 'email', 'version', 'is_active','role','accounts']
 
-        read_only_fields = ['role', 'is_active']
+        read_only_fields = ['role', 'is_active','version']
