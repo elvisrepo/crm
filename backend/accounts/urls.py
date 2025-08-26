d2792fefbe8b43b7cfbfb96eb67f39715df7c14e
elvisrepo <elvis.dev2@gmail.com>
2025-08-26 13:52:01 +0200

    accounts atomic transaction

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -3,8 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from accounts import views
 
 urlpatterns = [
-    path('accounts/', views.AccountList.as_view),
-    path('accounts/<int:pk>', views.AccountDetail.as_view)
+    path('accounts/', views.AccountList.as_view()),
+    path('accounts/<int:pk>', views.AccountDetail.as_view())
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -5,6 +5,8 @@ from .models import Account
 from .serializers import AccountSerializer
 from .permissions import IsAdminOrAccountOwner
 
+from django.db import transaction
+
     
 class AccountList(generics.ListCreateAPIView):
     queryset = Account.objects.all()
@@ -20,5 +22,20 @@ class AccountDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = AccountSerializer
     permission_classes = [IsAuthenticated, IsAdminOrAccountOwner]
 
+    @transaction.atomic
+    def update(self, request, *args, **kwargs):
+        return super().update(request, *args, **kwargs)
+    
+    @transaction.atomic
+    def destroy(self, request, *args, **kwargs):
+        ''' soft delete an account, is_active --> False'''  
+        instance = self.get_object() # accounts/5  get account with pk=5
+        instance.is_active = False
+        instance.save()
+
+        return Response(
+            {"message:":"Account deactivated successfully"},
+            status=status.HTTP_204_NO_CONTENT
+        )
    
         
